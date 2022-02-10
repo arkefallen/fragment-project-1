caa9212954f330e78db8da7a7f2c62284508e7bf
arkefallen <rafa9011@gmail.com>
2022-02-10 19:58:58 +0700

    add files to task1

--- app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
@@ -10,78 +10,78 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SimpleFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
-    private Button openBtn;
-
-    private Boolean isFragmentDisplayed = false;
-
-    private static final String STATE_FRAGMENT = "state_of_fragment";
-
-    private int mainActRadioButtonChoice = 2;
+//    private Button openBtn;
+//
+//    private Boolean isFragmentDisplayed = false;
+//
+//    private static final String STATE_FRAGMENT = "state_of_fragment";
+//
+//    private int mainActRadioButtonChoice = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        openBtn = findViewById(R.id.open_button);
-
-        if (savedInstanceState!=null) {
-            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
-        }
-
-        openBtn.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!isFragmentDisplayed) {
-                    showFragment();
-                }
-                else {
-                    closeFragment();
-                }
-            }
-        });
+//
+//        openBtn = findViewById(R.id.open_button);
+//
+//        if (savedInstanceState!=null) {
+//            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+//        }
+//
+//        openBtn.setOnClickListener( new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (!isFragmentDisplayed) {
+//                    showFragment();
+//                }
+//                else {
+//                    closeFragment();
+//                }
+//            }
+//        });
     }
 
-    // Memunculkan fragmen dengan dibungkus dahulu
-    private void showFragment() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        SimpleFragment simpleFragment = SimpleFragment.newInstance(mainActRadioButtonChoice);
-
-        fragmentTransaction.add(R.id.fragment_container, simpleFragment).addToBackStack(null).commit();
-
-        openBtn.setText(R.string.close);
-        isFragmentDisplayed = true;
-    }
-
-    private void closeFragment() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager.findFragmentById(R.id.fragment_container);
-
-        if(simpleFragment != null) {
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.remove(simpleFragment).commit();
-        }
-
-        openBtn.setText(R.string.open);
-        isFragmentDisplayed = false;
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        // menyimpan state fragment saat ini agar tidak tereset jika adanya perubahan konfigurasi
-        outState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
-        super.onSaveInstanceState(outState);
-    }
-
-
-    @Override
-    public void OnRadioButtonChoiceChecked(int choice) {
-        // menyimpan pilihan radio button yang nantinya akan dimunculkan lagi di fragment
-        mainActRadioButtonChoice = choice;
-        Toast.makeText(this, "Choice is" + Integer.toString(choice), Toast.LENGTH_SHORT).show();
-    }
+//    // Memunculkan fragmen dengan dibungkus dahulu
+//    private void showFragment() {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//        SimpleFragment simpleFragment = SimpleFragment.newInstance(mainActRadioButtonChoice);
+//
+//        fragmentTransaction.add(R.id.fragment_container, simpleFragment).addToBackStack(null).commit();
+//
+//        openBtn.setText(R.string.close);
+//        isFragmentDisplayed = true;
+//    }
+//
+//    private void closeFragment() {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager.findFragmentById(R.id.fragment_container);
+//
+//        if(simpleFragment != null) {
+//            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//            fragmentTransaction.remove(simpleFragment).commit();
+//        }
+//
+//        openBtn.setText(R.string.open);
+//        isFragmentDisplayed = false;
+//    }
+//
+//    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        // menyimpan state fragment saat ini agar tidak tereset jika adanya perubahan konfigurasi
+//        outState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
+//        super.onSaveInstanceState(outState);
+//    }
+//
+//
+//    @Override
+//    public void OnRadioButtonChoiceChecked(int choice) {
+//        // menyimpan pilihan radio button yang nantinya akan dimunculkan lagi di fragment
+//        mainActRadioButtonChoice = choice;
+//        Toast.makeText(this, "Choice is" + Integer.toString(choice), Toast.LENGTH_SHORT).show();
+//    }
 }
--- app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -24,44 +24,44 @@ import org.w3c.dom.Text;
 public class SimpleFragment extends Fragment {
 
     // Kondisi pilihan yang dipilih user di fragment
-    private static final int YES = 0;
-    private static final int NO = 1;
-    private static final int NONE = 2;
+//    private static final int YES = 0;
+//    private static final int NO = 1;
+//    private static final int NONE = 2;
 
     // Variabel untuk menyimpan pilihan radio button
-    private int mainActCurrentChoice = NONE;
+//    private int mainActCurrentChoice = NONE;
 
     // Di attach ke MainActivity ( host ) untuk ngasih tau state fragment
-    private OnFragmentInteractionListener mainActListener;
-    interface OnFragmentInteractionListener {
-        void OnRadioButtonChoiceChecked(int choice);
-    }
+//    private OnFragmentInteractionListener mainActListener;
+//    interface OnFragmentInteractionListener {
+//        void OnRadioButtonChoiceChecked(int choice);
+//    }
 
     // method ini dipanggil saat fragment nya ditempelkan ke activity
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
+//    @Override
+//    public void onAttach(@NonNull Context context) {
+//        super.onAttach(context);
+//
+//        // Cek apakah host activity udah implementasi interface onRadioButtonChoiceChecked
+//        if ( context instanceof OnFragmentInteractionListener ) {
+//            mainActListener = (OnFragmentInteractionListener) context;
+//        }
+//        else {
+//            throw new ClassCastException(context.toString()
+//                + getResources().getString(R.string.exception_message));
+//        }
+//    }
 
-        // Cek apakah host activity udah implementasi interface onRadioButtonChoiceChecked
-        if ( context instanceof OnFragmentInteractionListener ) {
-            mainActListener = (OnFragmentInteractionListener) context;
-        }
-        else {
-            throw new ClassCastException(context.toString()
-                + getResources().getString(R.string.exception_message));
-        }
-    }
+//    private static final String CHOICE = "choice";
 
-    private static final String CHOICE = "choice";
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    // TODO: Rename parameter arguments, choose names that match
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
 
     public SimpleFragment() {
         // Required empty public constructor
@@ -77,21 +77,20 @@ public class SimpleFragment extends Fragment {
      */
     // TODO: Rename and change types and number of parameters
     public static SimpleFragment newInstance(int choice) {
-        SimpleFragment fragment = new SimpleFragment();
-        Bundle arguments = new Bundle();
-        arguments.putInt(CHOICE,choice);
-        fragment.setArguments(arguments);
-        return fragment;
+        //        Bundle arguments = new Bundle();
+//        arguments.putInt(CHOICE,choice);
+//        fragment.setArguments(arguments);
+        return new SimpleFragment();
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
 
     // Tempat buat ngeload fragment nya dimana
     @Override
@@ -100,38 +99,38 @@ public class SimpleFragment extends Fragment {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
 
-        RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
-
-        if (getArguments().containsKey(CHOICE)) {
-            mainActCurrentChoice = getArguments().getInt(CHOICE);
-            if (mainActCurrentChoice != NONE ) {
-                radioGroup.check(radioGroup.getChildAt(mainActCurrentChoice).getId());
-            }
-        }
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton checkedButton = radioGroup.findViewById(checkedId);
-                int idx = radioGroup.indexOfChild(checkedButton);
-                TextView textView = rootView.findViewById(R.id.fragment_header);
-
-                if ( idx == YES  ) {
-                    textView.setText(R.string.yes_message);
-                    mainActCurrentChoice = YES;
-                    mainActListener.OnRadioButtonChoiceChecked(YES);
-                }
-                else if(idx == NO) {
-                    textView.setText(R.string.no_message);
-                    mainActCurrentChoice = NO;
-                    mainActListener.OnRadioButtonChoiceChecked(NO);
-                }
-                else {
-                    mainActCurrentChoice = NONE;
-                    mainActListener.OnRadioButtonChoiceChecked(NONE);
-                }
-            }
-        });
+//        RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+//
+//        if (getArguments().containsKey(CHOICE)) {
+//            mainActCurrentChoice = getArguments().getInt(CHOICE);
+//            if (mainActCurrentChoice != NONE ) {
+//                radioGroup.check(radioGroup.getChildAt(mainActCurrentChoice).getId());
+//            }
+//        }
+//
+//        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                RadioButton checkedButton = radioGroup.findViewById(checkedId);
+//                int idx = radioGroup.indexOfChild(checkedButton);
+//                TextView textView = rootView.findViewById(R.id.fragment_header);
+//
+//                if ( idx == YES  ) {
+//                    textView.setText(R.string.yes_message);
+//                    mainActCurrentChoice = YES;
+//                    mainActListener.OnRadioButtonChoiceChecked(YES);
+//                }
+//                else if(idx == NO) {
+//                    textView.setText(R.string.no_message);
+//                    mainActCurrentChoice = NO;
+//                    mainActListener.OnRadioButtonChoiceChecked(NO);
+//                }
+//                else {
+//                    mainActCurrentChoice = NONE;
+//                    mainActListener.OnRadioButtonChoiceChecked(NONE);
+//                }
+//            }
+//        });
 
         return rootView;
     }
